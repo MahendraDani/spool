68c93ebe492d4b6cc2344987d290a0f71a25de63
Mahendra Dani <danimahendra0904@gmail.com>
2025-05-08 16:31:36 +0530

    feat: manually generate a username for login with github
    Generate a random username from user's name and a random between 1 to 10000 and add it manually to database using better-auth database hooks.
    
    Signed-off-by: Mahendra Dani <danimahendra0904@gmail.com>

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,61 +1,86 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import {prisma} from "@/lib/prisma" 
+import { prisma } from "@/lib/prisma";
 import { openAPI, username } from "better-auth/plugins";
+import slugify from "@sindresorhus/slugify";
+
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 10000) + 1;
+};
+
+const generateUsername = (format: string) => {
+  return slugify(format) + generateRandomNumber();
+};
 
 export const auth = betterAuth({
-    appName : "Spool",
+  appName: "Spool",
 
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // update active session every 1 day
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      enabled: true,
+    },
+  },
+  account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ["github"],
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 8,
+    maxPasswordLength: 128,
+    autoSignIn: true,
+    requireEmailVerification: true,
+    sendResetPassword: async ({ user, url }) => {
+      // TODO : send email in PROD
+      console.log(`Reset password link : ${url} for`, user);
+    },
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    expiresIn: 60 * 60, // 1 hour
+    sendVerificationEmail: async ({ user, token }) => {
+      const verificationUrl = `${process.env.BETTER_AUTH_URL}/api/auth/verify-email?token=${token}&callbackURL=${process.env.EMAIL_VERIFICATION_CALLBACK_URL}`;
+      console.log(user);
+      console.log(verificationUrl);
+      // TODO : send email in PROD
+    },
+  },
+  rateLimit: {
+    enabled: true,
+  },
+  databaseHooks: {
+    user: {
+      create: {
+        // add username manually for social providers
+        before: async (user, ctx) => {
+          if (ctx?.request?.url.includes("github")) {
+            // add username manually here
+            const username = generateUsername(user.name);
+            return {
+              data: { ...user, username, displayUsername: username },
+            };
+          }
+        },
+      },
+    },
+  },
+  plugins: [
+    openAPI(),
+    username({
+      minUsernameLength: 5,
+      maxUsernameLength: 30,
     }),
-    session : {
-        expiresIn : 60 * 60 * 24 * 7, // 7 days
-        updateAge : 60 * 60 * 24 // update active session every 1 day
-    },
-    socialProviders : {
-        github : {
-            clientId : process.env.GITHUB_CLIENT_ID as string,
-            clientSecret : process.env.GITHUB_CLIENT_SECRET as string,
-            enabled : true,
-        },
-    },
-    account : {
-        accountLinking : {
-            enabled : true,
-            trustedProviders : ["github"]
-        }
-    },
-    emailAndPassword : {
-        enabled : true,
-        minPasswordLength : 8,
-        maxPasswordLength : 128,
-        autoSignIn : true,
-        requireEmailVerification : true,
-        sendResetPassword : async ({user,url})=>{
-            // TODO : send email in PROD
-            console.log(`Reset password link : ${url} for`, user);
-        }
-    },
-    emailVerification : {
-        sendOnSignUp : true,
-        autoSignInAfterVerification : true,
-        expiresIn : 60 * 60, // 1 hour
-        sendVerificationEmail : async ({user, token}) => {
-            const verificationUrl = `${process.env.BETTER_AUTH_URL}/api/auth/verify-email?token=${token}&callbackURL=${process.env.EMAIL_VERIFICATION_CALLBACK_URL}`;
-            console.log(user)
-            console.log(verificationUrl);
-            // TODO : send email in PROD
-        },
-    },
-    rateLimit : {
-        enabled : true
-    },
-    plugins : [
-        openAPI(),
-        username({
-            minUsernameLength : 5,
-            maxUsernameLength : 30,
-        })
-    ]
+  ],
 });
