e1af81a8197743a574a2763db90f94e86b280be7
Mahendra Dani <danimahendra0904@gmail.com>
2025-05-09 15:31:32 +0530

    feat: create custom API error handler
    extend the built-in Error class, to handle errors and
    create error route handlers to throw-catch errors gently.
    
    Signed-off-by: Mahendra Dani <danimahendra0904@gmail.com>

--- lib/api/with-session.ts
+++ lib/api/with-session.ts
@@ -2,6 +2,8 @@ import { } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "../auth";
 import {User, Session,} from "better-auth"
+import { SpoolAPIError } from "./error";
+import { spoolAPIErrorHandler, spoolInternalAPIErrorHandler } from "./error-handler";
 
 type TSessionRouteHandler = ({
   user,
@@ -16,16 +18,24 @@ export const withSession = async (
   handler: TSessionRouteHandler
 ): Promise<NextResponse> => {
   try {
+    if(!req.headers.get("Cookie")){
+      throw new SpoolAPIError({
+        status : "bad_request",
+        message : "Missing authorization header. Please add auth token in Cookie header."
+      })
+    }
     const session = await auth.api.getSession({headers : req.headers});
     if(!session){
-      throw new Error("no session")
+      throw new SpoolAPIError({
+        status : "unauthorized",
+        message : "Invalid or incorrect auth token in request header."
+      })
     }
     return await handler({session : session.session, user : session.user});
-  } catch (e) {
-    console.error("Error in withSession:", e);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+  } catch (error) {
+    if(error instanceof SpoolAPIError){
+      return spoolAPIErrorHandler(req,error);
+    }
+    return spoolInternalAPIErrorHandler(req,error);
   }
 };
