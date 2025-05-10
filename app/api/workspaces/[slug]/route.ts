006f1e571a584bc561dcdbdf88f967a523a78983
Mahendra Dani <danimahendra0904@gmail.com>
2025-05-10 16:53:18 +0530

    feat: DELETE /api/workspaces/<slug> api endpoint

--- app/api/workspaces/[slug]/route.ts
+++ app/api/workspaces/[slug]/route.ts
@@ -1,5 +1,8 @@
 import { SpoolAPIError } from "@/lib/api/error";
-import { spoolAPIErrorHandler, spoolInternalAPIErrorHandler } from "@/lib/api/error-handler";
+import {
+  spoolAPIErrorHandler,
+  spoolInternalAPIErrorHandler,
+} from "@/lib/api/error-handler";
 import { withSession } from "@/lib/api/with-session";
 import { prisma } from "@/lib/prisma";
 import { ZSlugSchema } from "@/lib/zod";
@@ -25,7 +28,7 @@ export const GET = async (
       const workspace = await prisma.workspace.findUnique({
         where: {
           slug,
-          ownerId : user.id
+          ownerId: user.id,
         },
         include: {
           folders: {
@@ -47,15 +50,57 @@ export const GET = async (
         },
       });
 
-      if(!workspace){
+      if (!workspace) {
         throw new SpoolAPIError({
-          status : "not_found",
-          message : "Workspace with provided slug not found."
-        })
+          status: "not_found",
+          message: "Workspace with provided slug not found.",
+        });
       }
       return NextResponse.json({
         data: workspace,
         message: "Workspace fetched successfully",
+      });
+    } catch (err) {
+      if (err instanceof ZodError) {
+        console.error(err);
+        return NextResponse.json({ error: err.flatten() });
+      } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        return NextResponse.json(
+          { error: "database_error", details: err.message },
+          { status: 500 }
+        );
+      } else if (err instanceof SpoolAPIError) {
+        return spoolAPIErrorHandler(req, err);
+      }
+      return spoolInternalAPIErrorHandler(req, err);
+    }
+  });
+};
+
+// DELETE /api/workspaces/<slug> delete workspace using its slug
+// This will delete all the folders and snippets associated in the workspace
+export const DELETE = async (
+  req: NextRequest,
+  props: { params: Promise<{ slug: string }> }
+) => {
+  return withSession(req, async ({ user }) => {
+    try {
+      const params = await props.params;
+
+      const { slug } = await ZSlugSchema.parseAsync({
+        slug: params.slug,
+      });
+
+      // Prisma will delete associated folders and snips automatically due to onCascade constraint
+      await prisma.workspace.delete({
+        where: {
+          slug,
+          ownerId: user.id,
+        },
+      });
+      return NextResponse.json({
+        data: null,
+        message: "Workspace deleted successfully",
       });
     } catch (err) {
       if (err instanceof ZodError) {
