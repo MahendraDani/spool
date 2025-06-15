b184056a94d4ccb597c818510710239bb132b6fa
Mahendra Dani <danimahendra0904@gmail.com>
2025-06-15 23:17:40 +0530

    feat: error handling in new folder feature
    Implemented error handling in folder feature by showing the error
    message in toast.
    
    Signed-off-by: Mahendra Dani <danimahendra0904@gmail.com>

--- lib/api/error-handler.ts
+++ lib/api/error-handler.ts
@@ -1,25 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 import { SpoolAPIError } from "./error";
 
-export const spoolAPIErrorHandler = (req : NextRequest, err : SpoolAPIError)=>{
+export const spoolAPIErrorHandler = (req: NextRequest, err: SpoolAPIError) => {
   console.log(`Error on: ${req.url}\tmethod: ${req.method}`);
   console.log(err);
-  return NextResponse.json({
-    error : {
-      status : err.status,
-      message : err.message,
-    }
-  })
-}
+  return NextResponse.json(
+    {
+      error: {
+        status: err.status,
+        message: err.message,
+      },
+    },
+    { status: err.statusCode, statusText: err.message }
+  );
+};
 
-export const spoolInternalAPIErrorHandler = (req : NextRequest, err : Error | unknown, message ?:string)=>{
+export const spoolInternalAPIErrorHandler = (
+  req: NextRequest,
+  err: Error | unknown,
+  message?: string
+) => {
   console.info(`Error on: ${req.url}\tmethod: ${req.method}`);
   console.error(err);
   const defaultError = "Oops, internal server error!";
-  return NextResponse.json({
-    error : {
-      status : "internal_server_error",
-      message : message ? message : defaultError,
-    }
-  })
-}
+  return NextResponse.json(
+    {
+      error: {
+        status: "internal_server_error",
+        message: message ? message : defaultError,
+      },
+    },
+    { status: 500, statusText : "Internal server error"}
+  );
+};
